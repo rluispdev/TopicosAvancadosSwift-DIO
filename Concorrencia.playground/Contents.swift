6ed783e13d66717f422f00c6e5c1422c7573a416
rluispdev <rluisp@icloud.com>
2024-12-04 19:27:08 -0300

    Finalizar parte 2 de concorrencia

--- Concorrencia.playground/Contents.swift
+++ Concorrencia.playground/Contents.swift
@@ -88,3 +88,123 @@ Task{
     let sortedNames = photoNames.sorted()
     let name = sortedNames[0]
 }
+
+
+//Mover uma foto de uma galeria para outra
+
+func add(_ photo: String, toGallery: String) {
+    print("Adicionando a foto \(photo) na galeria \(toGallery)")
+}
+
+func remove(_ photo: String, fromGallery: String) {
+    print("Remove a foto \(photo) na galeria \(fromGallery)")
+}
+
+Task {
+    let firstPhoto = await listPhotos(inGallery: "Summer Vacation")[0]
+    add(firstPhoto, toGallery: "Road Trip")
+///Neste ponto, o firstPhoto está temporariamente em ambas as galerias
+    remove(firstPhoto, fromGallery: "Summer Vacation")
+}
+
+///refatorando em uma func Sync, assim ela nao podera conter possiveis pontos de suspensao, nao vai ficar aramazenada em 2 lugares ao mesmo tempo, no futuro se tentar add codigo concorrente a essa func ela vai retornar um erro em tempo de compilacao ao inves de bug
+func move(_ photoName: String, from source: String, to destination: String) {
+    add(photoName, toGallery: destination)
+    remove(photoName, fromGallery: source)
+}
+
+Task {
+    let handle = FileHandle.standardInput
+    for try await line in handle.bytes.lines {
+        print(line)
+    }
+}
+
+
+func downloadPhoto(named: String) async -> Data {
+    ///Simular chamada ao backEnd
+    do {
+        try await Task.sleep(until: .now + .seconds(2), clock: .continuous)
+    }catch{}
+        return Data()
+    }
+
+Task{
+    
+    let photosNames = await listPhotos(inGallery: "Summer Vacation")
+
+    async let firstPhoto = downloadPhoto(named: photosNames[0])
+    async let secondPhoto =  downloadPhoto(named: photosNames[1])
+    async let thirdPhoto =  downloadPhoto(named: photosNames[2])
+    
+    let photos = await [ firstPhoto, secondPhoto, thirdPhoto]
+}
+
+
+//Tarefas em grupo de tarefas - Simulteanidade Estruturada
+
+Task{
+    await withTaskGroup(of: Data.self) { taskGroup in
+        let photoNames = await listPhotos(inGallery: "Summer Vacation")
+        for name in photoNames {
+            taskGroup.addTask{ await downloadPhoto(named: name)}
+        }
+        
+    }
+}
+
+
+Task {
+    let photo = await listPhotos(inGallery: "Summer Vacation")[0]
+    let handle = Task {
+        return await downloadPhoto(named: photo)
+    }
+    let result = await handle.value
+}
+
+
+//Actores
+
+actor TemparatureLogger {
+    let label: String
+    var measurements: [Int]
+    private(set) var max: Int
+    
+    init( label: String, measurements: Int) {
+        self.label = label
+        self.measurements = [measurements]
+        self.max = measurements
+    }
+}
+
+Task{
+    let logger = TemparatureLogger(label: "Ao ar livre", measurements: 25)
+    print(await logger.max)
+}
+
+extension TemparatureLogger {
+    func update(with measurement: Int) {
+        measurements.append(measurement)
+        if measurement > max {
+            max = measurement
+        }
+    }
+}
+
+
+struct TemperatureReading: Sendable {
+    var measurement: Int
+}
+
+extension TemparatureLogger {
+    func addReading(frm reading: TemperatureReading){
+        measurements.append(reading.measurement)
+    }
+}
+
+
+Task{
+    let logger = TemparatureLogger(label: "Chaleira", measurements: 45)
+    let reading = TemperatureReading(measurement: 45)
+    await logger.addReading(frm: reading)
+}
