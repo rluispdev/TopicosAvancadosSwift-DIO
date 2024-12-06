d7ae2acc3cc3e8ac72c0b12110f5c6e1adc9b26a
rluispdev <rluisp@icloud.com>
2024-12-06 19:33:51 -0300

    Criar  Protocolo com inicializador, subclasse e tipo existencial

--- Protocolos/Protocolos.playground/Contents.swift
+++ Protocolos/Protocolos.playground/Contents.swift
@@ -37,3 +37,66 @@ var b = SimpleClass()
 b.adjust()
 
 let bDescription = a.simpleDescription
+
+
+
+
+/// Protocolo com inicializador
+protocol SomeProtocol {
+    init(someParameter: Int)  }
+
+class SomeClass: SomeProtocol {
+    required init(someParameter: Int) {
+        //implementacao
+    }
+}
+
+
+//Inicialziando com uma subclass
+protocol AnotherProtocol {
+    init()
+}
+
+class SomeSuperClass {
+    init(){
+        //Implementacao
+    }
+}
+
+class SomeSubClass: SomeSuperClass, AnotherProtocol {
+    required override init() {
+        super.init()
+    }
+}
+
+
+//Tipo completo, tipo existencial
+
+protocol RandomNumberProtocol {
+    func random() -> Double
+}
+
+class RandomNumber: RandomNumberProtocol{
+    func random() -> Double {
+        return Double.random(in: 1.0...100.9)
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberProtocol
+    
+    init(sides: Int, generator: RandomNumberProtocol) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var ramdom = Dice(sides: 6, generator: RandomNumber())
+for _ in 1...5 {
+    print("O lançamento de dados aleatorios é: \(ramdom.roll())")
+}
