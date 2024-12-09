c4227db3ae7463ad0e9dd6d487bddcd494bb6d82
rluispdev <rluisp@icloud.com>
2024-12-09 19:21:35 -0300

    Executar acoes e sobrecargas de inicializadores

--- POO_Swift.playground/Contents.swift
+++ POO_Swift.playground/Contents.swift
@@ -41,3 +41,58 @@ class WhiteWall {
 
 //Criando um objeto Wall
 var whiteWall = WhiteWall()
+
+//Passando paramentros
+ 
+class BlackWall {
+    var length: Double
+    init(length: Double){
+        self.length = length
+        print("Length", length)
+    }
+}
+
+var blackWall = BlackWall(length: 10.5)
+
+//Executar uma determinada acao
+
+class LongWall{
+    var length: Double
+    var heigth: Double
+    
+    init(length: Double, height: Double) {
+        self.length = length
+        self.heigth = height
+        print("A area é \(calculateArea())")
+    }
+    func calculateArea() -> Double {
+        return length * heigth
+    }
+}
+
+var longWall = LongWall(length: 20.5, height: 8.6)
+var longWall2 = LongWall(length: 34.9, height: 78.0)
+
+//Sobrecarga de Inicializadores
+
+class PersonAge {
+    var age: Int
+    init() {
+        age = 20
+    }
+    
+    init(age: Int) {
+        self.age = age
+    }
+    
+    func getAge() -> Int {
+        return age
+    }
+    
+}
+
+var personAge = PersonAge()
+print("PersonAge: \(personAge.getAge())")
+
+var personAge2 = PersonAge(age: 34)
+print("PersonAge2 : \(personAge2.getAge())")
