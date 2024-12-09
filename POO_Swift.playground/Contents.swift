2e08d74ad25b1d36827c9f0a4e57b2213b1f2389
rluispdev <rluisp@icloud.com>
2024-12-09 19:40:04 -0300

    Inicilizador de conveniencia e com falha

--- POO_Swift.playground/Contents.swift
+++ POO_Swift.playground/Contents.swift
@@ -96,3 +96,58 @@ print("PersonAge: \(personAge.getAge())")
 
 var personAge2 = PersonAge(age: 34)
 print("PersonAge2 : \(personAge2.getAge())")
+
+
+// Inicilizador de conveniencia
+
+class University{
+    var name: String
+    var rank: String
+    //Init primario
+    init(name: String, rank: String) {
+        self.name = name
+        self.rank = rank
+    }
+    
+//init secundario
+    convenience init() {
+        self.init(name: "Universidade Federal de Juiz de Fora", rank: "1º")
+    }
+}
+
+var university = University()
+print(university.name)
+
+
+// Inicializador com falha.
+
+class File {
+    var folder: String
+    
+    init?(folder: String){
+        if folder.isEmpty {
+            print("Folder Not Found")
+            return nil
+        }
+        self.folder = folder
+    }
+}
+
+var file = File(folder: "")
+if  file != nil {
+    print("Arquivo encontrado com sucesso.")
+}else {
+    print("Erro o buscar o arquivo.")
+}
+
+
+// Inicializadores em Struct - Não obrigados
+
+struct PersonStruct {
+    var name: String
+    var age: Int
+    
+}
+
+var personStruct = PersonStruct(name: "Pedro", age: 23)
+print("Nome:", personStruct.name, "| Idade:", personStruct.age)
