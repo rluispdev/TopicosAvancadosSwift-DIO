ae629fffef057b0c4b093f80ac1ab1f914019c77
rluispdev <rluisp@icloud.com>
2024-12-10 20:23:41 -0300

    Protocolos e Conformidades

--- ProgramacaoOrientadaProtocolo.playground/Contents.swift
+++ ProgramacaoOrientadaProtocolo.playground/Contents.swift
@@ -1,1 +1,27 @@
- 
+ import UIKit
+
+//Protocolos e Conformidades
+
+protocol Bird {
+    var name: String {get}
+    var canFly: Bool {get}
+}
+
+protocol Flyable {
+    var maximumSpeed: Double { get}
+}
+
+struct Parrot : Bird, Flyable {
+    let name: String
+    let amplitude: Double
+    let frequency: Double
+    let canFly: Bool = true
+    
+    var maximumSpeed: Double {
+        3 * amplitude * frequency
+    }
+}
+
+let parrot = Parrot(name: "Papagaio", amplitude: 3.0, frequency: 5.0)
+print("O \(parrot.name) pode voar na velocidade de \(parrot.maximumSpeed) km/h")
+
