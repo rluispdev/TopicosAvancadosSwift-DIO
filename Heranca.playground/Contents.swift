1fff137c5472b852e6c87ecf835302dc1b872807
rluispdev <rluisp@icloud.com>
2024-12-10 19:21:37 -0300

    Overraide

--- Heranca.playground/Contents.swift
+++ Heranca.playground/Contents.swift
@@ -20,11 +20,46 @@ class Person {
     func play(sport: String){
         print("Esportes\(sport)")
     }
+    
+    //Sobrecarga de método
+    func play(instrument: String) {
+        print("Instrumento: \(instrument)")
+    }
 }
 
 class Men: Person{
     
 }
 
-let andy = Men(age: 20, gender: "Marculino", color: "Branca", matrialStatus: "Casado")
+let andy = Men(age: 20, gender: "Masculino", color: "Branca", matrialStatus: "Casado")
 print(andy.gender)
+
+
+//Substituicao de metodo - override
+
+class FootballPlayer: Person {
+    override func play(sport: String) {
+        print("Jogador de \(sport)")
+    }
+}
+let footballPlayer = FootballPlayer(age: 31, gender: "masculino", color: "parda", matrialStatus: "solteiro")
+footballPlayer.play(sport: "Futebol")
+
+class Pilot: Person {
+    override func play(sport: String){
+        print("Piloto de \(sport)")
+    }
+}
+
+let pilot = Pilot(age: 22, gender: "Masculino", color: "Negra", matrialStatus: "solteiro")
+pilot.play(sport: "Aviao")
+
+
+class Rock: Person {
+    override func play(instrument: String){
+        print("Toca \(instrument)")
+    }
+}
+
+let rock = Rock(age: 34, gender: "Masculino", color: "Branco", matrialStatus: "solteiro")
+rock.play(instrument: "guitarra")
