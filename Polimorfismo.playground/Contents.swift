ef17fc2fea8969d51f01187836f8a50b79dc0789
rluispdev <rluisp@icloud.com>
2024-12-10 19:41:31 -0300

    Polimorfismo

---
+++ Polimorfismo.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+class Player {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func play() {
+        print("Comportamento padrão")
+    }
+}
+
+class Batsman: Player {
+    override func play() {
+        bat()
+    }
+    
+    private func bat() {
+        print("\(name) está rebatendo.")
+    }
+}
+
+
+class Bowler: Player {
+    
+    override func play() {
+      bowl()
+    }
+    
+    private func bowl(){
+        print("\(name) está jogando a bola.")
+    }
+}
+
+
+class CricketTeam {
+    let name: String
+    let team: [Player]
+    
+    init(name: String, team: [Player]) {
+        self.name = name
+        self.team = team
+    }
+    
+    func play() {
+        team.forEach{ $0.play()}
+    }
+}
+
+let maria = Batsman(name: "Maria")
+let pedro = Bowler(name: "Pedro")
+let rioCricket = CricketTeam(name: "Rio Cricket", team: [pedro, maria])
+rioCricket.play()
+
